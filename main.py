09dab385c6c6f4c31d7b51bcfc36a7030c44c1b4
JaakM <116648173+JaakM@users.noreply.github.com>
2022-12-21 05:30:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,150 +1,118 @@
-import pygame, random   #impordin pygame mooduli ja random mooduli
+
+import pygame  #impordin pygame mooduli ja random mooduli
+import sys   #impordin sys mooduli
 pygame.init()   #käivitan pygame mooduli
 
 # värvid
-lBlue = [150, 204, 255]   #omastan muutujale "lblue" RGB väärtuse 150, 204, 255.
+lBlue = [176, 244, 250]   #omastan muutujale "lblue" RGB väärtuse 150, 204, 255.
 black = [0, 0, 0]
-# ekraani seaded
-screenX = 640   # määran ekraani x-telje suuruseks 640px
-screenY = 480   # määran ekraani y-telje suuruseks 480px
-screen = pygame.display.set_mode([screenX, screenY])   # omastan muutujale "screen" väärtused (väärtusteks on muutujad screenX ja screenY)
-pygame.display.set_caption("Ülesanne 6 - Jaak Mõttus")   # lisan mängu päisesse ülesande numbri ja enda nime
-#screen.fill(lBlue)  #täidan ekraani helesinise värviga, RGB 150, 284, 255
+
+#ekraan
+screenX = 640   # määran ekraani x-telje suuruseks 640px (akna kõrgus)
+screenY = 480   # määran ekraani y-telje suuruseks 480px (akna laius)
+screen = pygame.display.set_mode([screenX, screenY])   #omastan muutujale "screen" väärtused (muutujad screenX, screenY)
+pygame.display.set_caption("Ülesanne 6 - Jaak Mõttus")   #lisan mängu päisesse ülesande numbri ja enda nime
+screen.fill(lBlue)  #täidan ekraani helesinise värviga, RGB 150, 284, 255
 clock = pygame.time.Clock()   #omastan muutujale "clock" kella
-posX, posY = 0, 0   # muutuja "posX" algkordinaat on 0 ja muutuja "posY" algkordinaat on 0.
-speedX, speedY = 3, 4   #X-teljeline kiirus on 3 ja Y teljeline kiirus on 4
-speedT = 5
-score = 0   #NB!!!!!! "score = 0" peab olema kindlasti ennem while funktsiooni !!
-padMovingDirection = 0
-padSpeed = 7
+
+posX, posY = 0, 0   #muutuja "posX" algkordinaat on 0 ja muutuja "posY" algkordinaat on 0
+alusX, alusY = 200, screenY/1.5   #muutuja alusX algkordinaat on 200 ja
+#muutuja alusY kordinaat on Y-tleje maksimum väärtus jagatud 1.5 ehk 320
+speedX, speedY = 4, 5   #X-teljeline kiirus on 3 ja Y teljeline kiirus on 4
+score = 0   #omastan muutujale "score" väärtuse 0
+liikumise_suund = 0   #omastan muutujale "liikumise_suund väärtuse 0
+
+#tausta muusika
+pygame.mixer.music.load("taust3.wav")   #laadin muusika
+pygame.mixer.music.play(1)  #panen eelnevalt laaditud muusika mängima, 0 tähendab, et lugu hakkab korduma
+pygame.mixer.music.set_volume(0.2)   #muudan helitugevust 80% peale
 
 
-gameover = False
-while not gameover:   # kuni mäng ei ole läbi
-    clock.tick(60)    # uuendatakse ekraani 60 korda sekundis
+while True:   #panen whlie loppi käima ehk ekraani uuendama
+    clock.tick(60)    # ekraani uuendatakse 60 korda sekundis
 
     # mängu sulgemine ristist
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        break
+    sisend = pygame.event.poll()
+    for sisend in pygame.event.get():
+        if sisend.type == pygame.QUIT:   #kui sisendi tüüp on võrdne pygame quitiga
+            sys.exit()   #siis mängust väljutakse
 
-    # pall ja palli liikumine
+    # heliefektid
+    pygame.mixer.music.set_volume(0.2)   #muudan helitugevust 80% peale
+    porge = pygame.mixer.Sound("porge.ogg")
+    aluselt_porge = pygame.mixer.Sound("porge.ogg")
+    game_over = pygame.mixer.Sound("felldown.wav")
+
+
+    #alus
+    alusepilt = pygame.image.load("pad.png")   #laadin pad.png pildi ja omastan selle muutujale "alusepilt"
+    alusepilt = pygame.transform.scale(alusepilt, [120, 20])  #muudan muutuja "alusepilt" suuruseks 120*20px
+    alus = pygame.Rect(alusX, alusY, 120, 20)  #tekitan meetodi "rect" abil ristküliku, asukoht alusx, alusY, suurus 120*20px
+    screen.blit(alusepilt, alus)  # panen blit-ga ekraanile alusepildi aluse peale
+
+    # pall
     pallipilt = pygame.image.load("ball.png")   #omastan muutujale "pallipilt" png faili ball, mille laadin pygame.image.load abil
-    pall = pygame.Rect(posX, posY, 20, 20)   #omastan muutujale pall Rect funktsiooni, kusjuures Rect asukoht on märgitud muutujate posX ja posY abil (antud juhul on on x=0 ja y=0, sest skripti alguses sai sellised väärtused muutujatele posx ja posy omistatud), objekti Rect suurus on 20px korda 20px.
-    screen.blit(pallipilt, pall)   #kuvame screen.blit abil rectile, milleks on muutuja "pall", palli pildi "pallImage"
+    pall = pygame.Rect(posX, posY, 20, 20)   #omastan muutujale "pall" Rect meetodi abil loodud ristküliku asukohaga posX, posy ja suurusega 20*20px,
+    screen.blit(pallipilt, pall)   #kuvan blit abil ekraanile muutuja "pall" peale muutuja "pallipilt"
 
+    # palli liikumine
     posX += speedX   # muutuja posX väärtus suureneb muutuja speedX võrra
     posY += speedY   # muutuja posY väärtus suureneb muutuja speedY võrra
 
-    alusX = 200  # SIIA PANE HIIREGA LIIGUTAMINE
-    alusY = screenY / 1.5
+    if posX > screenX - pallipilt.get_rect().width or posX < 0:  #kui muutuja posX on suurem kui ekraani x-telje
+        # maksimaalne väärtus või kui muutuja posX on väiksem kui null (sisuliselt, kui posX on vahemikus 1 kuni 619)
+        speedX = -speedX   #siis muutuja "speedX" muutub negatiivseks (pall põrkab seinast).
+        pygame.mixer.Sound.play(porge)   #siis, kui pall seinaga kokku puutub, mängitakse seda heli faili "porge".
 
-    alusepilt = pygame.image.load("pad.png")
-    alus = pygame.Rect(alusX, alusY, 120, 20)
-    screen.blit(alusepilt, alus)
+    if posY < 0:  #kui palli y kordinaat on väiksem kui 0,
+        speedY = -speedY   #siis pall põrkab
+        pygame.mixer.Sound.play(porge) #mängitakse heli faili "porge"
 
+    # palli põrkamine aluse pealt
+    if pall.colliderect(alus) and posY > 0:   #kui pall puutub kokku alusega ja posY on suurem kui 0
+        speedY = -speedY   #siis speedY muutub negatiivseks ehk pall põrkab
+        score += 1   #ja score suureneb 1 võrra
+        pygame.mixer.Sound.play(aluselt_porge)   #mängitakse heli faili "aluselt_põrge"
 
-
-    if posX > screenX - pallipilt.get_rect().width or posX < 0:  #kui muutuja posX on suurem kui ekraani x-telje maksimaalne väärtus või kui muutuja posX on võiksem kui null (sisuliselt, kui posX on vahemikus 0 kuni 640)
-        speedX = -speedX   #siis muutuja "speedX" muutub negatiivseks (pall põrkab seinast). See tähendab, et muutuja "speedX" saab rahulikult speedX eelnevalt omistatud väärtuse võrra suureneda, kuni if lauses kirjeldatu saavutatakse (muutuja "posX" läheb suuremaks kui 640 või väiksemaks kui 0).
-
-    #if posY > screenY - pallipilt.get_rect().height or posY < 0:   # NB! pane siin 1.5 asemele aluse aadress? Kuidas x-telge määrata aluselt põrkamisel?
-     #   speedY = -speedY
-
-    if posY < 0:  #kui palli y kordinaat on väiksem kui 0, siis kiirus läheb negatiivsseks ehk pall põrkab
-        speedY = -speedY   #pall põrkab
-
-
-
+    if pall.colliderect(alus) and speedY > 0.5:   #kui pall puutub kokku alusega ja speedY on suurem kui 0.5
+        speedY = -speedY   #siis speedY muutub negatiivseks ehk pall põrkab
+        score -= 1   #ja score väheneb 1 võrra
 
 
-    for sisend in pygame.event.get():
-        if sisend.type == pygame.QUIT:
-                Gameover = True
-                exit()
+    #nooltega aluse juhtimine x-teljel
+    if sisend.type == pygame.KEYDOWN:   #kui mängija vajutab klavhi alla
+        if sisend.key == pygame.K_RIGHT:   #kui mängija vajutab nool paremale klahvi
+            liikumise_suund = "paremale"   #siis omastakse liikumise_suunale "paremale" väärtus
+        elif sisend.key == pygame.K_LEFT:   #kui mängija vajutab nool vasakule klahvi
+             liikumise_suund = "vasakule"   #siis omastatakse liikumise_suunale "vasakule" väärtus
 
-    if sisend.type == pygame.KEYDOWN:
-        if sisend.key == pygame.K.RIGHT:
-            padMovingDirection = "Parem"
-        elif sisend.key == pygame.K.LEFT:
-            padMovingDirection = "Vasak"
+    if sisend.type == pygame.KEYUP:   #kui mängija tõstab klahvi ülesse
+        if sisend.key == pygame.K_RIGHT:  #kui lahti lastud klahv on parem
+            liikumise_suund = 0   #siis liikumine on 0 ehk seisab paigal
+        elif sisend.key == pygame.K_LEFT:   #kui lahti lastud klahv on vasak
+            liikumise_suund = 0   #siis liikumine on 0 ehk seisab paigal
 
-    if sisend.type == pygame.KEYUP:
-        if sisend.key == pygame.K.RIGHT:
-            padMovingDirection = 0
-        elif sisend.key == pygame.K.LEFT:
-            padMovingDirection = 0
+    if liikumise_suund == "vasakule":   #kui liikumise_suund võrdub vasakule
+        if alusX > 0:   #ja aluse x kordinaat on suurem kui 0
+            alusX -= 10   #siis aluse x kordinaat väheneb 10px võrra iga ekraani uuendusega (iga loopiga)
+    if liikumise_suund == "paremale":   #kui liikumise:suund võrdub paremale
+        if alusX +120 < screenX:   #ja kui aluse x kordinaat + 120px on väiksem kui ekraani maks x kordinaat
+             alusX += 10   #siis aluse x kordinaat suureneb 10 px võrra iga ekraani uuendusega
 
-    #if pall.colliderect(alus) and speedY > 0:
-    #   speedY = -speedY
+    if posY == 440:   #kui pall y kordinaat on 440
+        pygame.mixer.Sound.play(game_over)   #mängi heli "game_over"
 
+    if posY == screenY:   #kui pall puutub alumist äärt
+        sys.exit()   #siis mäng lõpetatakse
 
-    if padMovingDirection == "Parem":
-        if padX + alusepilt.get_rect().widht < scrennX:
-            padX += padSpeed
-    elif padMovingDirection == "Vasak":
-        if padX > 0:
-            padX -= padSpeed
+    # Teksti seaded ja skoori kuvamine
+    game_font = pygame.font.Font("freesansbold.ttf", 24)   #omastan muutujale game_font fondi freesansbold suuruega 24px
+    text = game_font.render("SKOOR: " + str(score), True, black)  #omastan muutujale text sõna SKOOR, millele liidan
+    # striimina score väärtuse ja see kõik on musta värvi
+    screen.blit(text, (20,20))   #kuvan blit abil sreen peale texti, asukoht 20px x-telg ja 20px y telg
 
-
-
-
-
-    '''if posY == alusX and alusY:
-        speedY = -speedY'''
-
-
-
-    '''score = 0
-    if posY == screenY/1.5:  # see number (antud juhl screnYja 1.5 jagatis) peab lema põrkelaua Y-teljeline asukoht (kui muutuja posY on võrdne põrkelaua Y kordinaadiga, siis juhtub järgmisel real olev
-        score += 1
-        SKOOR = int(score)
-        print(str(SKOOR))'''
-
-        #def score(score):
-        #font = pygame.font.Font(pygame.font.match_font('Daytona'), 30)
-        #text = font.render("SKOOR: " + str(score), True, [0,0,0])
-        #screen.blit(text, [0, 0])   #kuvame ekraanile muutuja "text" ja kuvamise asukohaks on x=opx ja y=o px e
-
-    '''score = 0
-    if posY == 320:
-            score += 1'''
-
-    '''score('text')'''
-    '''score = 0
-    k = pygame.image.load("ball.png")
-    if posY == 320:
-        score += 1
-        screen.blit(str(score), (30, 0))'''
-
-    #score
-    '''score_value = 0
-    font = pygame.font.Font('freesansbold.ttf', 28)
-    textX = 10
-    textY = 10
-    if posY == 310:
-        score_value = score_value + 1
-    def show_score(x, y):
-        score = font.render("SKOOR: " + str(score_value), True, (0, 0, 0))
-        screen.blit(score, (x, y))
-    if posY == 310:
-        score_value = score_value + 1
-    show_score(textX, textY)'''
+    pygame.display.flip()   #kuvab kõik, mis sellele eelneb ehk ekraani värskendamine
+    screen.fill(lBlue)   #täidan ekraani eelnevalt muutujale "lBlue" omistatud värviga
+    # see vajalik, et peale palli liikumist ei jääks näha kollane teekond ehk palli pildid eelmistest kaadritest
 
 
-# text variables
-    #score = 0
-    game_font = pygame.font.Font("freesansbold.ttf", 24)
-
-    text = game_font.render("SKOOR: " + f"{score}", True, black)  #  str() asemel on kasutatud f{} ja toimib .
-    screen.blit(text, (20,20))
-
-    if posY >= screenY-19:
-        score += 1
-
-    print(score)   # see peab olema lõpus, aga ennem flippimist
-
-    pygame.display.flip()   #kuvab kõik, mis sellele eelneb
-    screen.fill(lBlue)   #täidan ekraani helesinise värviga, RGB 150, 284, 255, et peale palli liikumist ei jääks näha kollane teekon ehk vanad palli pildid.
-
-pygame.quit()
